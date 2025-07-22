aaab6d0a0a1c85038e53402b51bacd0f8118d212
tulgaa-12 <e7016307@gmail.com>
2025-07-22 14:58:48 +0800

    update

--- src/app/page.js
+++ src/app/page.js
@@ -1,103 +1,82 @@
-import Image from "next/image";
+"use client";
+import { useState } from "react";
+import { Container1 } from "./Container-1";
+import { Container2 } from "./Container-2";
+import { Container3 } from "./Container-3";
+import { Container } from "./Container";
 
-export default function Home() {
+const Join = () => {
+  const components = [Container1, Container2, Container3, Container];
+  const [step, setStep] = useState(0);
+  const Stepper = components[step];
+
+  const [container1, setContainer1] = useState({
+    firstname: "",
+    lastname: "",
+    username: "",
+    email: "",
+    phone: "",
+    password: "",
+    confirmpassword: "",
+    birthdate: null,
+    file: null,
+  });
+
+  const [error, setError] = useState({});
+
+  const NextHandler = () => {
+    setStep((prev) => prev + 1);
+  };
+  const BackHandler = () => {
+    setStep((prev) => prev - 1);
+  };
+
+  const uberTragen = (event) => {
+    console.log(event.target.value);
+    if (event.target.name === "firstname") {
+      setContainer1((prev) => {
+        return { ...prev, firstname: event.target.value };
+      });
+    } else if (event.target.name === "lastname") {
+      setContainer1((prev) => {
+        return { ...prev, lastname: event.target.value };
+      });
+    } else if (event.target.name === "username") {
+      setContainer1((prev) => {
+        return { ...prev, username: event.target.value };
+      });
+    } else if (event.target.name === "email") {
+      setContainer1((prev) => {
+        return { ...prev, email: event.target.value };
+      });
+    } else if (event.target.name === "phone") {
+      setContainer1((prev) => {
+        return { ...prev, phone: event.target.value };
+      });
+    } else if (event.target.name === "password") {
+      setContainer1((prev) => {
+        return { ...prev, password: event.target.value };
+      });
+    } else if (event.target.name === "confirm") {
+      setContainer1((prev) => {
+        return { ...prev, confirmpassword: event.target.value };
+      });
+    }
+  };
+
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              src/app/page.js
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+    <div className="flex justify-center flex-row  mt-[50px]">
+      <Stepper
+        NextHandler={NextHandler}
+        BackHandler={BackHandler}
+        uberTragen={uberTragen}
+        setContainer1={setContainer1}
+        container1={container1}
+        error={error}
+        setError={setError}
+      />
     </div>
   );
-}
+};
+
+export default Join;
